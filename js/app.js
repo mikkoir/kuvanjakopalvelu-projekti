9624a838e0692a2306f2cf4bbb022c71b64dcfd3
Mikko Romppanen <mikkoir@ety-a2123b-07.edu.wks.metropolia.fi>
2018-11-28 10:30:29 +0200

    message

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
-function openNav() {
+openNav = () => {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("").style.marginLeft = "250px";
-}
 
-function closeNav() {
+};
+
+closeNav = () => {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("").style.marginLeft= "0";
-}
+};
 
+
+document.getElementById('avaa').addEventListener('click',openNav);
+document.getElementById('sulje').addEventListener('click',closeNav);
