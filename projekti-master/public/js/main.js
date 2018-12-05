58e00fdf68cad0f7f628895ae0347575027dc3db
Arttu Saarimaa <32095355+Xyphnos@users.noreply.github.com>
2018-12-05 13:32:56 +0200

    Add files via upload

---
+++ projekti-master/public/js/main.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const lomake = document.querySelector('#lomake');
+const lista = document.querySelector('#result');
+const list = document.querySelector('#imagelist');
+
+//cookies
+console.log(document.cookie);
+const userID = document.cookie.split('=')[1];
+console.log('userID is', userID);
+
+
+const lahetaLomake = (evt) => {
+  evt.preventDefault();
+  const fd = new FormData(lomake);
+  const asetukset = {
+    method: 'post',
+    body: fd,
+  };
+
+  fetch('/upload', asetukset).then((response) => {
+    return response.json();
+  }).then((json) => {
+    const polku = 'files/';
+    const thumbs = 'thumbs/';
+    lista.innerHTML = '';
+    json.forEach(item => {
+      const li = document.createElement('li');
+      if (item.mimetype.includes('image')) {
+        const kuva = document.createElement('img');
+        kuva.src = polku + item.ufile;
+        li.appendChild(kuva);
+      }
+      lista.appendChild(li);
+    });
+  });
+};
+
+
+const getImages222 = () => {
+  const fd = new FormData(lomake);
+  const asetukset2 = {
+    method: 'post',
+    body: fd,
+  };
+  fetch('/upload',asetukset2).then((response) => {
+    return response.json();
+  }).then((json) => {
+    console.log(json);
+    const polku = 'files/';
+    const thumbs = 'thumbs/';
+    lista.innerHTML = '';
+    json.forEach(item => {
+      const li = document.createElement('li');
+      if (item.mimetype.includes('image')) {
+        const kuva = document.createElement('img');
+        kuva.src = polku + item.ufile;
+        li.appendChild(kuva);
+      }
+      lista.appendChild(li);
+    });
+  });
+};
+
+lomake.addEventListener('submit', lahetaLomake);
